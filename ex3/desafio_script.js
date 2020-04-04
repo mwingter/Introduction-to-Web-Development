8456a9ca9970a263fafdd94f711a269e04c62ce3
Michelle Wingter <mwingter@gmail.com>
2020-04-03 21:53:35 -0300

    Desafio do ex 3 completo
    falta só o 1 do ex3

--- ex3/desafio_script.js
+++ ex3/desafio_script.js
@@ -4,9 +4,17 @@
 */
 
 // Ex 1 - Mude o style da tag <body> para outro tipo de fonte. 
+document.body.style.fontFamily = "Arial";
 
 
 // Ex 2 - Substitua os <span> (nickname, favorites, hometown) com as informações de algum personagem.
+document.getElementById("nickname").innerHTML = "Midoriya Izuku";
+document.getElementById("favorites").innerHTML = "Super heroes";
+document.getElementById("hometown").innerHTML = "Musutafu, Japan";
 
 
 // Ex 3 - Crie um novo elemento img e coloque uma imagem do personagem escolhido. E acrescente o elemento na página.
+var img = document.createElement("img");
+img.src = "mid.jpg"
+
+document.body.appendChild(img);
