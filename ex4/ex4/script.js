b1c43c1e2ffe39d1a3b231345dd2a4ef98f4e55a
Michelle Wingter <mwingter@gmail.com>
2020-04-09 23:10:48 -0300

    Questão 4 do exercício 4 finalizada
    falta só o desafio

--- ex4/ex4/script.js
+++ ex4/ex4/script.js
@@ -1,3 +1,25 @@
+/*
+	Aluno: Michelle Wingter da Silva
+    nUSP: 10783243
+*/
+
+//####### Ex a - Usando como servidor o GET do List <resourse> 
+fetch('https://reqres.in/api/unknown')
+.then(function(response){
+    //console.log(response);
+    return response.json();
+})
+.then(function(response){
+    //console.log(response.data); //response.data retorna um array
+    //como é um array, tenho q fazer um loop pra ir um a um inserindo os dados dentro do html
+    response.data.forEach(function(user){
+        nameList.push(user.name);
+        rgbList.push(user.color);
+    })
+})
+
+
+//####### Ex b - criação de um elemento de “caixa” de cor preta, com um texto dentro que informa o HEX e o nome dessa cor.
 var box = document.createElement("div");
 box.style.backgroundColor = "black";
 box.style.height = "200px";
@@ -6,8 +28,6 @@ box.style.color = "white";
 box.style.paddingRight = "20px";
 box.style.textAlign = "right"
 
-
-
 var text1 = document.createElement("p");
 var text2 = document.createElement("p");
 text1.innerText = "#000";
@@ -15,10 +35,11 @@ text2.innerText = "black";
 text1.style.margin = "0";
 text2.style.margin = "0";
 
-
 box.appendChild(text1);
 box.appendChild(text2);
 
+
+//####### Ex c - Criação de um botão na página, que irá trocar a cor da “caixa” do ec b para os valores que receber do servidor.
 var button = document.createElement("button");
 button.innerText = "Change Color";
 button.style.color = "purple";
@@ -31,8 +52,20 @@ button.style.height = "35px"
 document.body.appendChild(box);
 document.body.appendChild(button);
 
-button.addEventListener("click", clicar);
+button.addEventListener("click", clicar); //adicionando evento ao botão
+
+var nameList = []; //lista com nome das cores
+var rgbList = []; //lista com código rgb das cores
 
 function clicar(){
-    button.innerText = "clicou"
+    var randomNumber = Math.floor(Math.random() * (nameList.length)); //cada vez que clicar, gera um número entre 1 e o número de cores disponíveis no servidor
+    //console.log(nameList.length, randomNumber, nameList[randomNumber]);
+
+    if(nameList.length > 0){
+        box.style.backgroundColor = rgbList[randomNumber]
+        //console.log(box.style.backgroundColor, nameList[randomNumber], typeof(nameList[randomNumber]));
+
+        text1.innerText = rgbList[randomNumber];
+        text2.innerText = nameList[randomNumber];
+    }
 }
